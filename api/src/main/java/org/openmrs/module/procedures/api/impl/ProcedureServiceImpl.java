5f0435e66944db5ea205e0546dccdb9927164985
jecihjoy <jecihjoy@gmail.com>
2024-03-13 00:57:41 +0300

    (feat) Handle persistence of transient data for results and complications

--- api/src/main/java/org/openmrs/module/procedures/api/impl/ProcedureServiceImpl.java
+++ api/src/main/java/org/openmrs/module/procedures/api/impl/ProcedureServiceImpl.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import java.util.Optional;
 
 import org.openmrs.api.ConditionService;
+import org.openmrs.api.ObsService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.procedures.api.ProcedureService;
 import org.openmrs.module.procedures.api.dao.ProcedureDao;
@@ -26,6 +27,27 @@ public class ProcedureServiceImpl implements ProcedureService {
 	
 	@Override
 	public Procedure saveOrUpdate(Procedure procedure) {
+		handleProcedureComplications(procedure);
+		handleProcedureResults(procedure);
 		return procedureDao.saveOrUpdate(procedure);
 	}
+	
+	private void handleProcedureComplications(Procedure procedure) {
+		ConditionService service = Context.getConditionService();
+		procedure.getComplications().forEach(p -> {
+			if (p.getId() == null) {
+				service.saveCondition(p);
+			}
+		});
+	}
+	
+	private void handleProcedureResults(Procedure procedure) {
+		ObsService service = Context.getObsService();
+		procedure.getProcedureResults().forEach(ob -> {
+			if (ob.getId() == null) {
+				service.saveObs(ob, "");
+			}
+		});
+	}
+	
 }
